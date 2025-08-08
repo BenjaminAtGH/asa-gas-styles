a4f4d56557784c02b29cd521255e5f6aea7502ab
BenjaminAtGH <benosvaldo07@gmail.com>
2025-08-08 13:35:18 +0900

    Update ui.js

--- ui.js
+++ ui.js
@@ -1,16 +1,15 @@
 
-function setupUI(){
-    document.getElementById("btn").addEventListener("click",doStuff);
-        function doStuff(){
-            const userInfo = {
-                firstName: document.getElementById("nm").value,
-                lastName: document.getElementById("ls").value,
-                sheet: document.getElementById("sheet").value
-            };
-        }    
-            sendToServer(userInfo);
-            document.getElementById("nm").value = "";
-            document.getElementById("ls").value = "";
-            document.getElementById("sheet").value = "";
-}
+document.getElementById("btn").addEventListener("click",doStuff());
+    function doStuff(){
+        const userInfo = {
+            firstName: document.getElementById("nm").value,
+            lastName: document.getElementById("ls").value,
+            sheet: document.getElementById("sheet").value
+        };
+    }    
+        sendToServer(userInfo);
+        document.getElementById("nm").value = "";
+        document.getElementById("ls").value = "";
+        document.getElementById("sheet").value = "";
+
       
